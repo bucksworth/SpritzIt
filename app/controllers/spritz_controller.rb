97b59f7eb41aa4dc6d134b6b5dfd9c4536cf84af
Anshul Jain <mosquito242@gmail.com>
2014-04-12 23:56:26 -0700

    push

--- app/controllers/spritz_controller.rb
+++ app/controllers/spritz_controller.rb
@@ -1,20 +1,17 @@
 class SpritzController < ApplicationController
 def spritz_it
 	@length = Hash.new
-	@length['0-30'] = Spritz.select(:length).group(:length).having("length > 0 AND length < 31").count.values.sum
-	@length['31-60'] = Spritz.select(:length).group(:length).having("length > 30 AND length < 61").count.values.sum
-	@length['61-90'] = Spritz.select(:length).group(:length).having("length > 60 AND length < 91").count.values.sum
-	@length['91-120'] = Spritz.select(:length).group(:length).having("length > 90 AND length < 121").count.values.sum
-	@length['121-150'] = Spritz.select(:length).group(:length).having("length > 120 AND length < 151").count.values.sum
-	@length['150+'] = Spritz.select(:length).group(:length).having("length > 150 ").count.values.sum
+	@length['0-150'] = Spritz.select(:length).group(:length).having("length > 0 AND length < 31").count.values.sum
+	@length['151-300'] = Spritz.select(:length).group(:length).having("length > 150 AND length < 301").count.values.sum
+	@length['301-450'] = Spritz.select(:length).group(:length).having("length > 300 AND length < 451").count.values.sum
+	@length['451-600'] = Spritz.select(:length).group(:length).having("length > 450 AND length < 601").count.values.sum
+	@length['601-750'] = Spritz.select(:length).group(:length).having("length > 600 AND length < 751").count.values.sum
+	@length['750+'] = Spritz.select(:length).group(:length).having("length > 750 ").count.values.sum
 end
 
 def create
-    @sp = Spritz.create(:url => params[:url], :text => params[:text], :length => params[:text].length, :country => params[:country])
-end
+    @sp = Spritz.create(:url => params[:url].split('//')[1].split('/')[0], :text => params[:text], :length => params[:text].split(" ").size, :country => params[:country])
 
-def show
-    @text = Spritz.find(2).text.length
 end
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,6 @@ SpritzIt::Application.configure do
   config.assets.digest = true
   config.assets.initialize_on_precompile = false
 
-
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
